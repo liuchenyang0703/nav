100582ff02d2f26b449deb2dc44fce2f491da5dc
liuchenyang <2162059863@qq.com>
2025-06-13 14:04:58 +0800

    更新了文件

--- docs/.vitepress/theme/data.ts
+++ docs/.vitepress/theme/data.ts
@@ -1096,12 +1096,6 @@ export const NAV_DATA: NavData[] = [
         desc: 'IconPark图标库是一个通过技术驱动矢量图标样式的开源图标库，可以实现根据单一 SVG 源文件变换出多种主题，具备丰富的分类、更轻量的代码和更灵活的使用场景；致力于构建高质量、统一化、可定义的图标资源，让大多数人都能够选择适合自己的风格图标',
         link: 'https://iconpark.oceanengine.com/official'
       },
-      {
-        title: 'Emoji searcher',
-        icon: 'https://emoji.muan.co/appicon.png',
-        desc: 'Emoji 表情大全',
-        link: ''
-      }
     ]
   },
   {
